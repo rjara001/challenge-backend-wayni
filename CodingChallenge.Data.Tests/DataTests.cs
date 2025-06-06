352f5cb589818d31e4d15ad18a8c758a7c7580a9
Rodrigo Jara <rjara001@yahoo.com>
2025-06-06 19:13:41 +0200

    add new unit test and new shapes

--- CodingChallenge.Data.Tests/DataTests.cs
+++ CodingChallenge.Data.Tests/DataTests.cs
@@ -25,7 +25,7 @@ namespace CodingChallenge.Data.Tests
         [TestCase]
         public void TestResumenListaConUnCuadrado()
         {
-            var cuadrados = new List<IFormaGeometrica> {new Cuadrado(5))};
+            var cuadrados = new List<IFormaGeometrica> { new Cuadrado(5) };
 
             var resumen = FormaGeometrica.Imprimir(cuadrados, new Babel(TipoIdioma.Castellano));
 
@@ -85,8 +85,49 @@ namespace CodingChallenge.Data.Tests
             var resumen = FormaGeometrica.Imprimir(formas, new Babel(TipoIdioma.Castellano));
 
             Assert.AreEqual(
-                "<h1>Reporte de Formas</h1>2 Cuadrados | Area 29 | Perimetro 28 <br/>2 Círculos | Area 13.01 | Perimetro 18.06 <br/>3 Triángulos | Area 49.64 | Perimetro 51.6 <br/>TOTAL:<br/>7 formas Perimetro 97.66 Area 91.65",
+                "<h1>Reporte de Formas</h1>2 Cuadrados | Area 29 | Perimetro 28 <br/>2 Circulos | Area 13.01 | Perimetro 18.06 <br/>3 Triangulos | Area 49.64 | Perimetro 51.6 <br/>TOTAL:<br/>7 formas Perimetro 97.66 Area 91.65",
                 resumen);
         }
+
+        [TestCase]
+        public void TestResumenListaConRectangulo()
+        {
+            var formas = new List<IFormaGeometrica>
+            {
+                new Rectangulo(6, 4)
+            };
+
+            var resumen = FormaGeometrica.Imprimir(formas, new Babel(TipoIdioma.Frances));
+
+            Assert.AreEqual("<h1>Rapport de Formes</h1>1 Rectangle | Surface 24 | Périmètre 20 <br/>TOTAL:<br/>1 formes Périmètre 20 Surface 24", resumen);
+        }
+
+        [TestCase]
+        public void TestResumenConUnTrapecioEnCastellano()
+        {
+            var formas = new List<IFormaGeometrica>
+            {
+                new Trapecio(10, 6, 4, 5) // base mayor, base menor, altura, lado no paralelo
+            };
+
+            var resumen = FormaGeometrica.Imprimir(formas, new Babel(TipoIdioma.Castellano));
+
+            Assert.AreEqual("<h1>Reporte de Formas</h1>1 Trapecio | Area 32 | Perimetro 26 <br/>TOTAL:<br/>1 formas Perimetro 26 Area 32", resumen);
+
+        }
+
+        [TestCase]
+        public void TestResumenConUnTrapecioEnFrances()
+        {
+            var formas = new List<IFormaGeometrica>
+    {
+        new Trapecio(10, 6, 4, 5)
+    };
+
+            var resumen = FormaGeometrica.Imprimir(formas, new Babel(TipoIdioma.Frances));
+
+            Assert.AreEqual("<h1>Rapport de Formes</h1>1 Trapèze | Surface 32 | Périmètre 26 <br/>TOTAL:<br/>1 formes Périmètre 26 Surface 32", resumen);
+        }
+
     }
 }
